6dc5dd5ec08885f00769d6c9ab372234b9fbfb95
Jixiu Chang <JixiuChang2901@gmail.com>
2024-01-28 22:30:51 -0600

    First change. Added Buttons on corners of screen

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -44,5 +44,6 @@ dependencies {
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.compose.material3)
     implementation(libs.androidx.activity.compose)
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     debugImplementation(libs.compose.ui.tooling)
 }
--- androidApp/src/main/java/com/example/businessbudgettracker/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/businessbudgettracker/android/MainActivity.kt
@@ -13,16 +13,7 @@ import com.example.businessbudgettracker.Greeting
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingView(Greeting().greet())
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
